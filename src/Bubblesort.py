234148ba262f2197277e7f9479805dae203468c7
IAMJulioGil <JulioGICJ@gmail.com>
2025-03-30 15:05:52 -0600

    Agregando bubble sort

---
+++ src/Bubblesort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if not swapped: break
+
+# Dada una lista desordenada                
+x = [9, 8, 7, 5, 4, 3, 0]
+
+# La ordenamos utilizando bubble sort
+bubble_sort(x)
+print(x)
